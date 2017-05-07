3e5f39228edd6835a6a6d0d8f1528c17de5ef031
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-07 18:21:21 -0400

    v6.1 - Spring Tutorial 06 - Using Constructor Injection

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning-Spring
-That's just me learning the spring framework
+That's just me learning the spring framework. 
+
+The content here till version 24 will be mainly based on my learning from video series here: 
+https://www.youtube.com/playlist?list=PLC97BDEFDCDD169D7
--- src/com/nbhirud/spring/Triangle.java
+++ src/com/nbhirud/spring/Triangle.java
@@ -12,7 +12,29 @@ package com.nbhirud.spring;
 public class Triangle {
     
     private String type;
+    private int height;
 
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+    
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
     public String getType() {
         return type;
     }
@@ -23,7 +45,7 @@ public class Triangle {
     
     
     public void draw() {
-        System.out.println(getType()+" Triangle drawn");
+        System.out.println(getType()+" Triangle drawn of height "+ getHeight());
     }
     
 }
