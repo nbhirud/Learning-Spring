cb0bcb4b44e0d9380ba285f31ccb05d779daecfe
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-06 20:39:50 -0400

    v2.0

---
+++ src/com/nbhirud/spring/DrawingApp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nbhirud.spring;
+
+/**
+ *
+ * @author nbhirud
+ */
+public class DrawingApp {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.draw();
+    }
+}
