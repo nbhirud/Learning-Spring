b9d8494c901efa0441fc1018d8103c7d999afb5f
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-07 17:27:31 -0400

    v5.1 - replacing BeanFactory with ApplicatonContext. Skipped version numbers to match the video number

--- src/com/nbhirud/spring/DrawingApp.java
+++ src/com/nbhirud/spring/DrawingApp.java
@@ -7,6 +7,8 @@ package com.nbhirud.spring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -17,8 +19,9 @@ public class DrawingApp {
 
     public static void main(String[] args) {
         //Triangle triangle = new Triangle();
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle = (Triangle) factory.getBean("triangle");
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
 
         triangle.draw();
     }
