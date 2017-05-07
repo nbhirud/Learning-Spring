c24084a019f7a77e72229779e378613d84727696
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-07 17:35:47 -0400

    v5.2 - Property initialization

--- src/com/nbhirud/spring/Triangle.java
+++ src/com/nbhirud/spring/Triangle.java
@@ -10,8 +10,20 @@ package com.nbhirud.spring;
  * @author nbhirud
  */
 public class Triangle {
+    
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
     public void draw() {
-        System.out.println("Triangle drawn");
+        System.out.println(getType()+" Triangle drawn");
     }
     
 }
